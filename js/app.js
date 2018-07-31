9f4e4030c8dd6a24f25d94aefcfa93874d0e1262
msilva <marcia.silva1802@gmail.com>
2018-07-31 07:37:25 -0300

    LabCar V 1.0 finalizada

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-// La funcionalidad de tu proyecto
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -9.70854412787279, lng: -77.730489125},
+    zoom: 3.2
+  });
+}
